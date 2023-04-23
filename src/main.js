7487462ca1506ee51d5026419f1b1b2171e69392
Danila Avetisyan <avetisyandanila21@yandex.ru>
2023-04-23 10:46:45 +0300

    Убрал vuetify из проекта

--- src/main.js
+++ src/main.js
@@ -1,22 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
-const vuetify = createVuetify({
-    components,
-    directives,
-})
-
 // Vue router
 import { router } from "./router";
 
 // App
 createApp(App)
-    .use(vuetify)
     .use(router)
     .mount('#app')
