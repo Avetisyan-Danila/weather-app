08c344a8268e4eb6d05bd3b7f90c500e966b61e2
Danila Avetisyan <avetisyandanila21@yandex.ru>
2023-04-28 23:03:57 +0300

    Добавил дочерние маршруты для детальной страницы погоды в городе.

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,10 +3,24 @@ export const routes = [
         path: "/",
         name: "home",
         component: () => import("@/views/HomeView.vue"),
+        children: [
+            {
+                path: "/detailed/:city",
+                name: "detailed-home",
+                component: () => import("../views/DetailedView.vue"),
+            },
+        ]
     },
     {
         path: "/search",
         name: "search",
         component: () => import("@/views/SearchView.vue"),
+        children: [
+            {
+                path: "/detailed/:city",
+                name: "detailed-search",
+                component: () => import("../views/DetailedView.vue"),
+            },
+        ]
     },
 ];
