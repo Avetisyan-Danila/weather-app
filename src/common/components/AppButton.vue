b187ab1095991c07716c2a79339d599a465ac7af
Danila Avetisyan <avetisyandanila21@yandex.ru>
2023-04-29 13:39:28 +0300

    Создал переиспользуемые компоненты инпута и кнопки

---
+++ src/common/components/AppButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button
+    class="app-button"
+    :type="type"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup>
+defineProps({
+  type: {
+    type: String,
+    default: 'text'
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/ds-system/ds";
+@import "@/assets/scss/mixins/mixins";
+
+.app-button {
+  @include m-s16-h16;
+
+  height: 41px;
+  border-radius: 100px;
+
+  background: $purple-gradient;
+  border: none;
+  color: $white;
+  cursor: pointer;
+}
+</style>
---
+++ src/common/components/AppInput.vue
@@ -0,0 +1,86 @@
+<template>
+  <label class="app-input">
+    <input
+        :value="modelValue"
+        :type="type"
+        :name="name"
+        :placeholder="placeholder"
+        @input="emit('update:modelValue', $event.target.value)"
+    />
+  </label>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: null
+  },
+  type: {
+    type: String,
+    default: 'text'
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  placeholder: {
+    type: String,
+    default: ''
+  }
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const input = computed({
+  get () {
+    return props.modelValue
+  },
+  set (value) {
+    emit('update:modelValue', value)
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/ds-system/ds";
+@import "@/assets/scss/mixins/mixins";
+
+.app-input {
+  input {
+    box-sizing: border-box;
+    width: 100%;
+    height: 38px;
+    padding: 5px 20px 5px 50px;
+
+    @include r-s15-h15;
+    color: $white;
+    background: transparent;
+    border: none;
+    border-radius: 15px;
+    outline: none;
+  }
+
+  @include purple-gradient;
+
+  &:after {
+    border-radius: 15px;
+  }
+
+  &:before {
+    content: '';
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    left: 16px;
+
+    width: 16px;
+    height: 16px;
+
+    background-image: url(../../assets/img/icons/search.svg);
+    background-size: 100% 100%;
+  }
+}
+</style>
