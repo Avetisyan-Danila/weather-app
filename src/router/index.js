4ed233e1ea3931befd6762afa2a3a5b419491232
Danila Avetisyan <avetisyandanila21@yandex.ru>
2023-05-03 23:31:09 +0300

    Добавил скролл вверх при смене роута

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,9 @@ import { routes } from "./routes";
 export const router = createRouter({
     routes,
     history: createWebHistory(),
+    scrollBehavior () {
+        return { x: 0, y: 0 }
+    }
 });
 
 router.afterEach(async (to, from) => {
