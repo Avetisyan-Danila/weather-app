349a95a8de7818821725ea90d542c30cfdb924ae
Danila Avetisyan <avetisyandanila21@yandex.ru>
2023-05-06 17:14:01 +0300

    Изменил название функции для получения времени

--- src/stores/weather.js
+++ src/stores/weather.js
@@ -1,7 +1,8 @@
 import { defineStore } from "pinia";
 import { getCityWeather } from "@/services/forecast-service.js";
 import { normalizeWeatherInfo } from "@/common/helpers/normalizeWeatherInfo.js";
-import { getOneMinuteAgo } from "@/common/helpers/getOneMinuteAgo.js";
+import { getMinutesAgo } from "@/common/helpers/getMinutesAgo.js";
+import { ONE_MINUTE } from "@/common/constants/index.js";
 
 export const useWeatherStore = defineStore("weather", {
     state: () => ({
@@ -14,7 +15,7 @@ export const useWeatherStore = defineStore("weather", {
         async setCityWeather(cityName, days) {
             // Если погода для города уже есть, то делать повторные запрос, если данные получены более минуты назад
             if (this.cityWeather(cityName)) {
-                if (!getOneMinuteAgo(this.cityWeather(cityName).dateReceipt)) {
+                if (!getMinutesAgo(this.cityWeather(cityName).dateReceipt, ONE_MINUTE)) {
                     return
                 } else {
                     this.citiesWeather = this.citiesWeather.filter(city => city.location.name !== cityName)
