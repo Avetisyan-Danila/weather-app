458e7248a4c49294c7094566c32dcd5159997382
Danila Avetisyan <avetisyandanila21@yandex.ru>
2023-05-06 12:04:18 +0300

    Добавил константы для минут

--- src/common/constants/index.js
+++ src/common/constants/index.js
@@ -1,3 +1,7 @@
 export const API_KEY = '142881e823a2448c849171124230505';
 export const BASE_URL = 'http://api.weatherapi.com/v1';
+
 export const MAIN_CITY = 'Moscow';
+
+export const MINUTE_IN_SEC = 60;
+export const ONE_MINUTE = 1;
