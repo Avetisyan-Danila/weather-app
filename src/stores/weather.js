d0d208a84d5a856dee4c3d68ea057114273dbdf2
Danila Avetisyan <avetisyandanila21@yandex.ru>
2023-05-06 17:35:06 +0300

    Изменил название геттера в weather store

--- src/stores/weather.js
+++ src/stores/weather.js
@@ -1,5 +1,5 @@
 import { defineStore } from "pinia";
-import { getCityWeather } from "@/services/forecast-service.js";
+import { fetchCityWeather } from "@/services/forecast-service.js";
 import { normalizeWeatherInfo } from "@/common/helpers/normalizeWeatherInfo.js";
 import { getMinutesAgo } from "@/common/helpers/getMinutesAgo.js";
 import { ONE_MINUTE } from "@/common/constants/index.js";
@@ -9,20 +9,20 @@ export const useWeatherStore = defineStore("weather", {
         citiesWeather: [],
     }),
     getters: {
-        cityWeather: state => cityName => state.citiesWeather.find(city => city.location.name === cityName),
+        getCityWeather: state => cityName => state.citiesWeather.find(city => city.location.name === cityName),
     },
     actions: {
         async setCityWeather(cityName, days) {
             // Если погода для города уже есть, то делать повторные запрос, если данные получены более минуты назад
-            if (this.cityWeather(cityName)) {
-                if (!getMinutesAgo(this.cityWeather(cityName).dateReceipt, ONE_MINUTE)) {
+            if (this.getCityWeather(cityName)) {
+                if (!getMinutesAgo(this.getCityWeather(cityName).dateReceipt, ONE_MINUTE)) {
                     return
                 } else {
                     this.citiesWeather = this.citiesWeather.filter(city => city.location.name !== cityName)
                 }
             }
 
-            const cityWeather = await getCityWeather(cityName, days);
+            const cityWeather = await fetchCityWeather(cityName, days);
             this.citiesWeather.push(normalizeWeatherInfo(cityWeather));
         },
     }
