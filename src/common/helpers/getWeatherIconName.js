16826cf255d5f22776c021b2f8ed5810d106bf47
Danila Avetisyan <avetisyandanila21@yandex.ru>
2023-05-07 15:13:33 +0300

    Убрал лишние выводы в консоль

--- src/common/helpers/getWeatherIconName.js
+++ src/common/helpers/getWeatherIconName.js
@@ -1,9 +1,7 @@
 import weatherConditions from "@/common/enums/weatherConditions.js";
 
 export const getWeatherIconName = (code, time) => {
-    console.log(code, time)
     if (code === 1000) {
-        console.log(time > 6 && time < 22)
         return time >= 6 && time < 22 ? 'sunny' : 'clear';
     }
 
