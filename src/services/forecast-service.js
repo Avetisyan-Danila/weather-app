e1fb06b0c3e23d8d20fc4990223402d6b057c795
Danila Avetisyan <avetisyandanila21@yandex.ru>
2023-05-07 23:18:49 +0300

    Избавился от кэширования fetch запросов

--- src/services/forecast-service.js
+++ src/services/forecast-service.js
@@ -1,6 +1,6 @@
 import { API_KEY, BASE_URL } from "@/common/constants";
 
 export const fetchCityWeather = (city, days) => {
-    return fetch(`${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&days=${days}&aqi=no&alerts=no`)
+    return fetch(`${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&days=${days}&aqi=no&alerts=no?timestamp=${new Date().getTime()}`)
         .then((response) => response.json())
 }
