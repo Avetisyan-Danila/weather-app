accc7e63b85f40cda3ff6b301fbf7c9d199569b8
Danila Avetisyan <avetisyandanila21@yandex.ru>
2023-05-13 13:47:31 +0300

    Реализовал поиск города и добавил сохранение 6 последних искомых городов

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -8,7 +8,8 @@
             placeholder="Name of the city"
             v-model="searchValue"
             :is-valid="isValid"
-            :error-message="'Enter the name of the city'"
+            :empty-error-message="'Enter the name of the city'"
+            :search-error-message="searchError"
         />
       </form>
 
@@ -22,55 +23,90 @@
       </div>
     </div>
 
-    <div class="popular">
-      <ul class="popular__list">
-        <li class="popular__item">
-          <city-card
-              :to="{ name: 'detailed', params: { city: 'test' } }"
-              title="Surat"
-              weather="Sunny"
-          />
-        </li>
-        <li class="popular__item">
-          <city-card
-              :to="{ name: 'detailed', params: { city: 'test' } }"
-              title="Mumbai"
-              weather="Rain"
-          />
-        </li>
-        <li class="popular__item">
-          <city-card
-              :to="{ name: 'detailed', params: { city: 'test' } }"
-              title="Chennai"
-              weather="Thunder"
-          />
-        </li>
-      </ul>
-    </div>
+    <transition name="fade" appear>
+      <div
+          v-if="searchPageForecasts.length"
+          class="recent"
+      >
+        <div class="recent__title">Recent searches</div>
+        <ul class="recent__list">
+          <li
+              v-for="forecast in searchPageForecasts"
+              class="recent__item"
+          >
+            <transition name="list" appear>
+              <city-card
+                  :to="{ name: 'detailed', params: { city: forecast.location.name } }"
+                  :title="forecast.location.name"
+                  :weatherIcon="forecast.currentDayIcon"
+                  :weatherName="capitalizeFirstLetter(weatherConditions[forecast.current.condition.code])"
+              />
+            </transition>
+          </li>
+        </ul>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script setup>
-import { getImage } from "@/common/helpers/getImage.js";
-import { ref, watch } from "vue";
 import AppInput from "@/common/components/AppInput.vue";
 import CityCard from "@/modules/city-card/CityCard.vue";
+import weatherConditions from "@/common/enums/weatherConditions.js";
+import { getImage } from "@/common/helpers/getImage.js";
+import { onMounted, ref, watch } from "vue";
+import { useWeatherStore } from "@/stores/weather.js";
+import { capitalizeFirstLetter } from "@/common/helpers/capitalizeFirstLetter.js";
+import { useRecentSearches } from "@/stores/recentSearches.js";
+import { useRouter } from "vue-router";
 
 const isValid = ref(true);
 const searchValue = ref('');
+const searchError = ref('');
+
+const router = useRouter();
 
 watch(searchValue, () => {
   isValid.value = true;
 })
 
-const onSubmit = () => {
+const weatherStore = useWeatherStore();
+const recentSearchesStore = useRecentSearches();
+
+const searchPageForecasts = ref([]);
+const onSubmit = async () => {
   if (!searchValue.value) {
     isValid.value = false;
+    searchError.value = '';
     return;
   }
 
-  console.log('Success')
+  try {
+    recentSearchesStore.setRecentSearch(searchValue.value);
+    searchPageForecasts.value = [];
+    searchError.value = '';
+
+    await router.push({ name: 'detailed', params: {city: searchValue.value }});
+  } catch (e) {
+    searchError.value = 'Couldn`t find the city';
+  }
+
+  searchValue.value = '';
 }
+
+onMounted(async () => {
+  // Если существуют записи в localStorage, то получить погоду для город
+  if (localStorage.getItem('recentSearches')) {
+    recentSearchesStore.recentSearches = JSON.parse(localStorage.getItem('recentSearches'));
+
+    for (const city of recentSearchesStore.recentSearches) {
+      await weatherStore.setCityWeather(city, 1);
+      searchPageForecasts.value.push(weatherStore.getCityWeather(city));
+    }
+  } else {
+    localStorage.setItem('recentSearches', JSON.stringify([]));
+  }
+})
 </script>
 
 <style lang="scss" scoped>
@@ -80,7 +116,7 @@ const onSubmit = () => {
 .search {
   &__top {
     display: flex;
-    align-items: center;
+    align-items: flex-start;
     gap: 15px;
 
     padding: 0 4px;
@@ -92,9 +128,13 @@ const onSubmit = () => {
   }
 
   &__location {
+    right: 0;
+    top: 0;
+
     display: flex;
     justify-content: center;
     align-items: center;
+
     width: 38px;
     height: 38px;
     cursor: pointer;
@@ -107,7 +147,10 @@ const onSubmit = () => {
   }
 }
 
-.popular {
+.recent {
+  &__title {
+    margin-bottom: 20px;
+  }
   &__list {
     @include clear-list;
 
