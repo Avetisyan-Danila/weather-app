619d7114f6f4ac9807d668712d120493d772c91d
Danila Avetisyan <avetisyandanila21@yandex.ru>
2023-05-14 13:21:22 +0300

    Добавил первоначальную загрузку избранных городов в App.vue

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,16 @@
 <script setup>
 import AppLayout from "@/layouts/AppLayout.vue";
 import { useRecentSearchesStore } from "@/stores/recentSearches.js";
+import { useFavoritesStore } from "@/stores/favorites.js";
 
 const recentSearchesStore = useRecentSearchesStore();
+const favoritesStore = useFavoritesStore();
 
 // Загрузка первоначальных данных
 // Загружаем последние 6 город, которые искал пользователь
 void recentSearchesStore.initRecentStore();
 // Загружаем избранные города
-void recentSearchesStore.initRecentStore();
+void favoritesStore.initFavoritesStore();
 </script>
 
 <style lang="scss">
