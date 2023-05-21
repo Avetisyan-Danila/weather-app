f658b0230cec0bb4327a1ff153c02c0dae42c6f0
Danila Avetisyan <avetisyandanila21@yandex.ru>
2023-05-21 13:49:02 +0300

    Изменил константу MAIN_CITY

--- src/common/constants/index.js
+++ src/common/constants/index.js
@@ -1,9 +1,11 @@
+import {getUserCityName} from "@/common/helpers/getUserCityName.js";
+
 export const API_KEY = '142881e823a2448c849171124230505';
 export const WEATHER_BASE_URL = 'http://api.weatherapi.com/v1';
 
 export const USER_GEOLOCATION_BASE_URL = 'https://api.bigdatacloud.net/data/reverse-geocode-client';
 
-export const MAIN_CITY = 'Moscow';
+export const MAIN_CITY = await getUserCityName();
 
 export const MINUTE_IN_SEC = 60;
 export const ONE_MINUTE = 1;
